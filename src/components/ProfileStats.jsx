5eca7fc81a9a35b0f77e03640ff6966157feb819
laura-2035 <matiussifa@gmail.com>
2021-04-11 01:15:12 -0300

    Css changes

--- src/components/ProfileStats.jsx
+++ src/components/ProfileStats.jsx
@@ -5,9 +5,9 @@ export default props => {
     return (
         <div className="profile-card-stats">
             <ul className="profile-card-stats-list">
-                <li>{props.followers}</li>
-                <li>{props.likes}</li>
-                <li>{props.photos}</li>
+                <li className="profile-card-stats-list-item profile-followers"><p>{props.followers}</p><span>Followers</span></li>
+                <li className="profile-card-stats-list-item profile-likes"><p>{props.likes}</p><span>Likes</span></li>
+                <li className="profile-card-stats-list-item profile-photos"><p>{props.photos}</p><span>Photos</span></li>
             </ul>
         </div>
     )
